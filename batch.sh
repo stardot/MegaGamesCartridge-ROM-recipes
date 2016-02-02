1ba1ba48f2b804007cd7ab472c0cc0916bc32487
David Boddie <david@boddie.org.uk>
2016-02-02 18:49:21 +0100

    Updates to the batch creation script, including the addition of auto-boot options. Added the test script.

--- batch.sh
+++ batch.sh
@@ -3,223 +3,250 @@
 set -e
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/A-M/3DPool_E.uef ROMs/3DPool-1.rom ROMs/3DPool-2.rom
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/A-M/3DPool_E.uef ROMs/3DPool-1.rom ROMs/3DPool-2.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/A-M/Arcadians.uef ROMs/Arcadians-1.rom ROMs/Arcadians-2.rom
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/A-M/Arcadians.uef ROMs/Arcadians-1.rom ROMs/Arcadians-2.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py -w 39f ~/Games/UEFs/games/A-M/AstroPlumber_E.uef ROMs/AstroPlumber.rom
+./UEF2ROM.py -a -b -w 39f ~/Games/UEFs/games/A-M/AstroPlumber_E.uef ROMs/AstroPlumber.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/A-M/BarBilliards_E.uef ROMs/BarBilliards.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/A-M/Boffin2-modified_BE.uef ROMs/Boffin2-1.rom ROMs/Boffin2-2.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/A-M/Boxer.uef ROMs/Boxer-1.rom ROMs/Boxer-2.rom
-
-# *MGCROM: */
-./UEF2ROM.py -s -w 860 -t ~/Games/UEFs/games/A-M/Breakthrough_RUN_E.uef ROMs/Breakthrough-1.rom ROMs/Breakthrough-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 880 ~/Games/UEFs/games/A-M/BugBlast.uef ROMs/BugBlast-1.rom ROMs/BugBlast-2.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -s -m ~/Games/UEFs/games/A-M/BumbleBee_E.uef ROMs/BumbleBee-1.rom ROMs/BumbleBee-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 880 ~/Games/UEFs/games/A-M/CastleAssault_E.uef ROMs/CastleAssault.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -s -m ~/Games/UEFs/games/A-M/CastleOfRiddles_E.uef ROMs/CastleOfRiddles-1.rom ROMs/CastleOfRiddles-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 39f ~/Games/UEFs/games/A-M/CavemanCapers.uef ROMs/CavemanCapers.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m ~/Games/UEFs/games/A-M/ChuckieEgg_E.uef ROMs/ChuckieEgg.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/A-M/Citadel-PIASRR_E.uef ROMs/Citadel-1.rom ROMs/Citadel-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/A-M/CosmicCamouflage_Tape.uef ROMs/CosmicCamouflage-1.rom ROMs/CosmicCamouflage-2.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m ~/Games/UEFs/games/A-M/CrazyTracer_E.uef ROMs/CrazyTracer.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/A-M/CybertronMission_Tape.uef ROMs/CybertronMission.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/A-M/DangerUXB_Tape.uef ROMs/DangerUXB.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/A-M/DareDevilDenis.uef ROMs/DareDevilDenis.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 1000 ~/Games/UEFs/games/A-M/DeathStar.uef ROMs/DeathStar.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 39f ~/Games/UEFs/games/A-M/DrainMania.uef ROMs/DrainMania-1.rom ROMs/DrainMania-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/A-M/EMBA.uef ROMs/EMBA.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/A-M/Firebug_E.uef ROMs/Firebug-1.rom ROMs/Firebug-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -t -w 880 -s ~/Games/UEFs/games/A-M/Firetrack_E.uef ROMs/Firetrack-1.rom ROMs/Firetrack-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -s ~/Games/UEFs/games/A-M/Frak_Tape.uef ROMs/Frak-1.rom ROMs/Frak-2.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/A-M/Freefall_E.uef ROMs/Freefall-1.rom ROMs/Freefall-2.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/Gauntlet_E.uef ROMs/Gauntlet-1.rom ROMs/Gauntlet-2.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m ~/Games/UEFs/games/A-M/Go_E.uef ROMs/Go.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 39f -t ~/Games/UEFs/games/A-M/Guardian_E.uef ROMs/Guardian.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 860 ~/Games/UEFs/games/A-M/HorseRace.uef ROMs/HorseRace.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/A-M/Impact.uef ROMs/Impact-1.rom ROMs/Impact-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 39f -t -s ~/Games/UEFs/games/A-M/JetBootJack_E.uef ROMs/JetBootJack-1.rom ROMs/JetBootJack-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 39f ~/Games/UEFs/games/A-M/Joust.uef ROMs/Joust-1.rom ROMs/Joust-2.rom
-
-# *ROM: CHAIN""
-./UEF2ROM.py -m ~/Games/UEFs/games/A-M/JungleJive_Tape.uef ROMs/JungleJive.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -s ~/Games/UEFs/games/A-M/KarateCombat.uef ROMs/KarateCombat-1.rom ROMs/KarateCombat-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/A-M/KillerGorilla.uef ROMs/KillerGorilla.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 120 -s -t ~/Games/UEFs/games/A-M/KissinCousins_E.uef ROMs/KissinCousins-1.rom ROMs/KissinCousins-2.rom
+./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/BarBilliards_E.uef ROMs/BarBilliards.rom
 
 # *ROM: */
-./UEF2ROM.py -s -m ~/Games/UEFs/games/A-M/LastOfTheFree.uef ROMs/LastOfTheFree-1.rom ROMs/LastOfTheFree-2.rom
+./UEF2ROM.py -a -b -r -m -s ~/Games/UEFs/games/A-M/Boffin2-modified_BE.uef ROMs/Boffin2-1.rom ROMs/Boffin2-2.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m ~/Games/UEFs/games/A-M/LunarRescue_E.uef ROMs/LunarRescue.rom
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/A-M/Boxer.uef ROMs/Boxer-1.rom ROMs/Boxer-2.rom
+
+# *MGCROM: */
+./UEF2ROM.py -a -b -r -s -w 860 -t ~/Games/UEFs/games/A-M/Breakthrough_RUN_E.uef ROMs/Breakthrough-1.rom ROMs/Breakthrough-2.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py -w 880 ~/Games/UEFs/games/A-M/MagicMushrooms_Tape.uef ROMs/MagicMushrooms.rom
+./UEF2ROM.py -a -b -w 880 ~/Games/UEFs/games/A-M/BugBlast.uef ROMs/BugBlast-1.rom ROMs/BugBlast-2.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m ~/Games/UEFs/games/A-M/Maze_E.uef ROMs/Maze.rom
+./UEF2ROM.py -a -b -s -m ~/Games/UEFs/games/A-M/BumbleBee_E.uef ROMs/BumbleBee-1.rom ROMs/BumbleBee-2.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/A-M/Meteors_E.uef ROMs/Meteors.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 880 ~/Games/UEFs/games/A-M/Monsters.uef ROMs/Monsters.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 39f ~/Games/UEFs/games/A-M/MoonBuggy.uef ROMs/MoonBuggy-1.rom ROMs/MoonBuggy-2.rom
+./UEF2ROM.py -a -b -w 880 ~/Games/UEFs/games/A-M/CastleAssault_E.uef ROMs/CastleAssault.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/MoonRaider_E.uef ROMs/MoonRaider-1.rom ROMs/MoonRaider-2.rom
+./UEF2ROM.py -a -b -s -m ~/Games/UEFs/games/A-M/CastleOfRiddles_E.uef ROMs/CastleOfRiddles-1.rom ROMs/CastleOfRiddles-2.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/N-Z/Overdrive.uef ROMs/Overdrive.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 120 ~/Games/UEFs/games/N-Z/Pengi.uef ROMs/Pengi.rom
+./UEF2ROM.py -a -b -w 39f ~/Games/UEFs/games/A-M/CavemanCapers.uef ROMs/CavemanCapers.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/N-Z/PhilosophersQuest_E.uef ROMs/PhilosophersQuest-1.rom ROMs/PhilosophersQuest-2.rom
+./UEF2ROM.py -a -b -m ~/Games/UEFs/games/A-M/ChuckieEgg_E.uef ROMs/ChuckieEgg.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m ~/Games/UEFs/games/N-Z/PhilosophersStone_BE.uef ROMs/PhilosophersStone.rom
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/A-M/Citadel-PIASRR_E.uef ROMs/Citadel-1.rom ROMs/Citadel-2.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py -w 120 ~/Games/UEFs/games/N-Z/Pinball.uef ROMs/Pinball.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 39f -s -t ~/Games/UEFs/games/N-Z/Psycastria.uef ROMs/Psycastria-1.rom ROMs/Psycastria-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -s ~/Games/UEFs/games/N-Z/Quest.uef ROMs/Quest-1.rom ROMs/Quest-2.rom
+./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/CosmicCamouflage_Tape.uef ROMs/CosmicCamouflage-1.rom ROMs/CosmicCamouflage-2.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m ~/Games/UEFs/games/N-Z/Ravage_E.uef ROMs/Ravage.rom
+./UEF2ROM.py -a -b -m ~/Games/UEFs/games/A-M/CrazyTracer_E.uef ROMs/CrazyTracer.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py -s ~/Games/UEFs/games/N-Z/Repton.uef ROMs/Repton-1.rom ROMs/Repton-2.rom
+./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/CybertronMission_Tape.uef ROMs/CybertronMission.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py -s ~/Games/UEFs/games/N-Z/Repton2.uef ROMs/Repton2-1.rom ROMs/Repton2-2.rom
+./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/DangerUXB_Tape.uef ROMs/DangerUXB.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py -s ~/Games/UEFs/games/N-Z/Repton3.uef ROMs/Repton3-1.rom ROMs/Repton3-2.rom
+./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/DareDevilDenis.uef ROMs/DareDevilDenis.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/N-Z/RubbleTrouble.uef ROMs/RubbleTrouble-1.rom ROMs/RubbleTrouble-2.rom
+./UEF2ROM.py -a -b -w 1000 ~/Games/UEFs/games/A-M/DeathStar.uef ROMs/DeathStar.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/N-Z/Saracoid.uef ROMs/Saracoid-1.rom ROMs/Saracoid-2.rom
+./UEF2ROM.py -a -m ~/Games/UEFs/games/A-M/DiamondMine-MRM_E.uef ROMs/DiamondMine.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/Shark_E.uef ROMs/Shark-1.rom ROMs/Shark-2.rom
+./UEF2ROM.py -a ~/Games/UEFs/games/A-M/DiamondMine2-BlueRibbon_E.uef ROMs/DiamondMine2.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py -w 880 ~/Games/UEFs/games/N-Z/Sim_Tape.uef ROMs/Sim.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/N-Z/SmashAndGrab.uef ROMs/SmashAndGrab.rom
+./UEF2ROM.py -a -b -w 39f ~/Games/UEFs/games/A-M/DrainMania.uef ROMs/DrainMania-1.rom ROMs/DrainMania-2.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m ~/Games/UEFs/games/N-Z/Snooker-Acornsoft_E.uef ROMs/Snooker.rom
+./UEF2ROM.py -a ~/Games/UEFs/games/A-M/DAR-Draughts_E.uef ROMs/Draughts.rom
 
 # *MGCROM: CHAIN""
-./UEF2ROM.py -t -s ~/Games/UEFs/games/N-Z/SpacemanSid.uef ROMs/SpacemanSid-1.rom ROMs/SpacemanSid-2.rom
+./UEF2ROM.py -a -s -t -w 39f ~/Games/UEFs/games/A-M/Elixir_E.uef ROMs/Elixir-1.rom ROMs/Elixir-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/EMBA.uef ROMs/EMBA.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a ~/Games/UEFs/games/A-M/Erbert_E.uef ROMs/Erbert.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -s -m ~/Games/UEFs/games/N-Z/SphinxAdventure_E.uef ROMs/SphinxAdventure-1.rom ROMs/SphinxAdventure-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py ~/Games/UEFs/games/N-Z/StockCar.uef ROMs/StockCar-1.rom ROMs/StockCar-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 39f -t ~/Games/UEFs/games/N-Z/Stranded_E.uef ROMs/Stranded.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -s ~/Games/UEFs/games/N-Z/Syncron.uef ROMs/Syncron-1.rom ROMs/Syncron-2.rom
+./UEF2ROM.py -a -m ~/Games/UEFs/games/A-M/FelixAndTheFruitMonsters_E.uef ROMs/FelixAndTheFruitMonsters.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m ~/Games/UEFs/games/N-Z/TalesOfTheArabianNights_E.uef ROMs/TalesOfTheArabianNights-1.rom ROMs/TalesOfTheArabianNights-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -s ~/Games/UEFs/games/N-Z/Thrust_Tape.uef ROMs/Thrust-1.rom ROMs/Thrust-2.rom
+./UEF2ROM.py -a -m ~/Games/UEFs/games/A-M/FelixInTheFactory_E.uef ROMs/FelixInTheFactory.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -m -s ~/Games/UEFs/games/N-Z/TwinKingdomValley-modified_E.uef ROMs/TwinKingdomValley-1.rom ROMs/TwinKingdomValley-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -s ~/Games/UEFs/games/N-Z/TwelfthNight-Electron.uef ROMs/TwelfthNight-1.rom ROMs/TwelfthNight-2.rom
-
-# *MGCROM: CHAIN""
-./UEF2ROM.py -w 120 ~/Games/UEFs/games/N-Z/VideosRevenge.uef ROMs/VideosRevenge-1.rom ROMs/VideosRevenge-2.rom
+./UEF2ROM.py -a -m ~/Games/UEFs/games/A-M/FelixMeetsTheEvilWeevils_E.uef ROMs/FelixMeetsTheEvilWeevils-1.rom ROMs/FelixMeetsTheEvilWeevils-2.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -s -m ~/Games/UEFs/games/N-Z/Village.uef ROMs/Village-1.rom ROMs/Village-2.rom
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/A-M/Firebug_E.uef ROMs/Firebug-1.rom ROMs/Firebug-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -t -w 880 -s ~/Games/UEFs/games/A-M/Firetrack_E.uef ROMs/Firetrack-1.rom ROMs/Firetrack-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -s ~/Games/UEFs/games/A-M/Frak_Tape.uef ROMs/Frak-1.rom ROMs/Frak-2.rom
 
 # *ROM: CHAIN""
-./UEF2ROM.py -s -m ~/Games/UEFs/games/N-Z/WheelOfFortune_E.uef ROMs/WheelOfFortune-1.rom ROMs/WheelOfFortune-2.rom
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/A-M/Freefall_E.uef ROMs/Freefall-1.rom ROMs/Freefall-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/Gauntlet_E.uef ROMs/Gauntlet-1.rom ROMs/Gauntlet-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m ~/Games/UEFs/games/A-M/Go_E.uef ROMs/Go-1.rom ROMs/Go-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 39f -t ~/Games/UEFs/games/A-M/Guardian_E.uef ROMs/Guardian.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 860 ~/Games/UEFs/games/A-M/HorseRace.uef ROMs/HorseRace.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/A-M/Impact.uef ROMs/Impact-1.rom ROMs/Impact-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 39f -t -s ~/Games/UEFs/games/A-M/JetBootJack_E.uef ROMs/JetBootJack-1.rom ROMs/JetBootJack-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 39f ~/Games/UEFs/games/A-M/Joust.uef ROMs/Joust-1.rom ROMs/Joust-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m ~/Games/UEFs/games/A-M/JungleJive_Tape.uef ROMs/JungleJive.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -s ~/Games/UEFs/games/A-M/KarateCombat.uef ROMs/KarateCombat-1.rom ROMs/KarateCombat-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/KillerGorilla.uef ROMs/KillerGorilla.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 120 -s -t ~/Games/UEFs/games/A-M/KissinCousins_E.uef ROMs/KissinCousins-1.rom ROMs/KissinCousins-2.rom
+
+# *ROM: */
+./UEF2ROM.py -a -b -r -s -m ~/Games/UEFs/games/A-M/LastOfTheFree.uef ROMs/LastOfTheFree-1.rom ROMs/LastOfTheFree-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m ~/Games/UEFs/games/A-M/LunarRescue_E.uef ROMs/LunarRescue.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 880 ~/Games/UEFs/games/A-M/MagicMushrooms_Tape.uef ROMs/MagicMushrooms.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m ~/Games/UEFs/games/A-M/Maze_E.uef ROMs/Maze.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/Meteors_E.uef ROMs/Meteors.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 880 ~/Games/UEFs/games/A-M/Monsters.uef ROMs/Monsters.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 39f ~/Games/UEFs/games/A-M/MoonBuggy.uef ROMs/MoonBuggy-1.rom ROMs/MoonBuggy-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/MoonRaider_E.uef ROMs/MoonRaider-1.rom ROMs/MoonRaider-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b ~/Games/UEFs/games/N-Z/Overdrive.uef ROMs/Overdrive.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 120 ~/Games/UEFs/games/N-Z/Pengi.uef ROMs/Pengi.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/N-Z/PhilosophersQuest_E.uef ROMs/PhilosophersQuest-1.rom ROMs/PhilosophersQuest-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/N-Z/PhilosophersStone_BE.uef ROMs/PhilosophersStone-1.rom ROMs/PhilosophersStone-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 120 ~/Games/UEFs/games/N-Z/Pinball.uef ROMs/Pinball.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 39f -s -t ~/Games/UEFs/games/N-Z/Psycastria.uef ROMs/Psycastria-1.rom ROMs/Psycastria-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Quest.uef ROMs/Quest-1.rom ROMs/Quest-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m ~/Games/UEFs/games/N-Z/Ravage_E.uef ROMs/Ravage.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Repton.uef ROMs/Repton-1.rom ROMs/Repton-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Repton2.uef ROMs/Repton2-1.rom ROMs/Repton2-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Repton3.uef ROMs/Repton3-1.rom ROMs/Repton3-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a ~/Games/UEFs/games/A-M/DAR-Reversi_E.uef ROMs/Reversi.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b ~/Games/UEFs/games/N-Z/RubbleTrouble.uef ROMs/RubbleTrouble-1.rom ROMs/RubbleTrouble-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/N-Z/Saracoid.uef ROMs/Saracoid-1.rom ROMs/Saracoid-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/Shark_E.uef ROMs/Shark-1.rom ROMs/Shark-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 880 ~/Games/UEFs/games/N-Z/Sim_Tape.uef ROMs/Sim.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b ~/Games/UEFs/games/N-Z/SmashAndGrab.uef ROMs/SmashAndGrab.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m ~/Games/UEFs/games/N-Z/Snooker-Acornsoft_E.uef ROMs/Snooker.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -t -s ~/Games/UEFs/games/N-Z/SpacemanSid.uef ROMs/SpacemanSid-1.rom ROMs/SpacemanSid-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -s -m ~/Games/UEFs/games/N-Z/SphinxAdventure_E.uef ROMs/SphinxAdventure-1.rom ROMs/SphinxAdventure-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b ~/Games/UEFs/games/N-Z/StockCar.uef ROMs/StockCar-1.rom ROMs/StockCar-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 39f -t ~/Games/UEFs/games/N-Z/Stranded_E.uef ROMs/Stranded.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Syncron.uef ROMs/Syncron-1.rom ROMs/Syncron-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m ~/Games/UEFs/games/N-Z/TalesOfTheArabianNights_E.uef ROMs/TalesOfTheArabianNights-1.rom ROMs/TalesOfTheArabianNights-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Thrust_Tape.uef ROMs/Thrust-1.rom ROMs/Thrust-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/N-Z/TwinKingdomValley-modified_E.uef ROMs/TwinKingdomValley-1.rom ROMs/TwinKingdomValley-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -s -l ~/Games/UEFs/games/N-Z/TwelfthNight-Electron.uef ROMs/TwelfthNight-1.rom ROMs/TwelfthNight-2.rom
+
+# *MGCROM: CHAIN""
+./UEF2ROM.py -a -b -w 120 ~/Games/UEFs/games/N-Z/VideosRevenge.uef ROMs/VideosRevenge-1.rom ROMs/VideosRevenge-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -s -m ~/Games/UEFs/games/N-Z/Village.uef ROMs/Village-1.rom ROMs/Village-2.rom
+
+# *ROM: CHAIN""
+./UEF2ROM.py -a -b -s -m ~/Games/UEFs/games/N-Z/WheelOfFortune_E.uef ROMs/WheelOfFortune-1.rom ROMs/WheelOfFortune-2.rom
---
+++ testroms.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+set -e
+
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/3DPool-1.rom -rom1 ROMs/3DPool-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Arcadians-1.rom -rom1 ROMs/Arcadians-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/AstroPlumber.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/BarBilliards.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Boffin2-1.rom -rom1 ROMs/Boffin2-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Boxer-1.rom -rom1 ROMs/Boxer-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Breakthrough-1.rom -rom1 ROMs/Breakthrough-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/BugBlast-1.rom -rom1 ROMs/BugBlast-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/BumbleBee-1.rom -rom1 ROMs/BumbleBee-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/CastleAssault.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/CastleOfRiddles-1.rom -rom1 ROMs/CastleOfRiddles-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/CavemanCapers.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/ChuckieEgg.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Citadel-1.rom -rom1 ROMs/Citadel-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/CosmicCamouflage-1.rom -rom1 ROMs/CosmicCamouflage-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/CrazyTracer.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/CybertronMission.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/DangerUXB.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/DareDevilDenis.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/DeathStar.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/DrainMania-1.rom -rom1 ROMs/DrainMania-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/EMBA.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Firebug-1.rom -rom1 ROMs/Firebug-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Firetrack-1.rom -rom1 ROMs/Firetrack-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Frak-1.rom -rom1 ROMs/Frak-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Freefall-1.rom -rom1 ROMs/Freefall-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Gauntlet-1.rom -rom1 ROMs/Gauntlet-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Go-1.rom -rom1 ROMs/Go-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Guardian.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/HorseRace.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Impact-1.rom -rom1 ROMs/Impact-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/JetBootJack-1.rom -rom1 ROMs/JetBootJack-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Joust-1.rom -rom1 ROMs/Joust-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/JungleJive.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/KarateCombat-1.rom -rom1 ROMs/KarateCombat-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/KillerGorilla.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/KissinCousins-1.rom -rom1 ROMs/KissinCousins-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/LastOfTheFree-1.rom -rom1 ROMs/LastOfTheFree-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/LunarRescue.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/MagicMushrooms.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Maze.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Meteors.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Monsters.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/MoonBuggy-1.rom -rom1 ROMs/MoonBuggy-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/MoonRaider-1.rom -rom1 ROMs/MoonRaider-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Overdrive.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Pengi.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/PhilosophersQuest-1.rom -rom1 ROMs/PhilosophersQuest-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/PhilosophersStone-1.rom -rom1 ROMs/PhilosophersStone-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Pinball.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Psycastria-1.rom -rom1 ROMs/Psycastria-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Quest-1.rom -rom1 ROMs/Quest-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Ravage.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Repton-1.rom -rom1 ROMs/Repton-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Repton2-1.rom -rom1 ROMs/Repton2-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Repton3-1.rom -rom1 ROMs/Repton3-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/RubbleTrouble-1.rom -rom1 ROMs/RubbleTrouble-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Saracoid-1.rom -rom1 ROMs/Saracoid-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Shark-1.rom -rom1 ROMs/Shark-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Sim.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/SmashAndGrab.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Snooker.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/SpacemanSid-1.rom -rom1 ROMs/SpacemanSid-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/SphinxAdventure-1.rom -rom1 ROMs/SphinxAdventure-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/StockCar-1.rom -rom1 ROMs/StockCar-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Stranded.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Syncron-1.rom -rom1 ROMs/Syncron-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/TalesOfTheArabianNights-1.rom -rom1 ROMs/TalesOfTheArabianNights-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Thrust-1.rom -rom1 ROMs/Thrust-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/TwinKingdomValley-1.rom -rom1 ROMs/TwinKingdomValley-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/TwelfthNight-1.rom -rom1 ROMs/TwelfthNight-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/VideosRevenge-1.rom -rom1 ROMs/VideosRevenge-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Village-1.rom -rom1 ROMs/Village-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/WheelOfFortune-1.rom -rom1 ROMs/WheelOfFortune-2.rom
