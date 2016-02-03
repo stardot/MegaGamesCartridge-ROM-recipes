b38c3f720b853e270a04140b74f160ac92678cf8
David Boddie <david@boddie.org.uk>
2016-02-03 01:13:06 +0100

    More updates.

--- batch.sh
+++ batch.sh
@@ -23,7 +23,7 @@ set -e
 ./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/DareDevilDenis.uef ROMs/DareDevilDenis.rom
 ./UEF2ROM.py -a -b -w 1000 ~/Games/UEFs/games/A-M/DeathStar.uef ROMs/DeathStar.rom
 ./UEF2ROM.py -a -m ~/Games/UEFs/games/A-M/DiamondMine-MRM_E.uef ROMs/DiamondMine.rom
-./UEF2ROM.py -a ~/Games/UEFs/games/A-M/DiamondMine2-BlueRibbon_E.uef ROMs/DiamondMine2.rom
+./UEF2ROM.py -a -w 39f ~/Games/UEFs/games/A-M/DiamondMine2-BlueRibbon_E.uef ROMs/DiamondMine2.rom
 ./UEF2ROM.py -a -b -w 39f ~/Games/UEFs/games/A-M/DrainMania.uef ROMs/DrainMania-1.rom ROMs/DrainMania-2.rom
 ./UEF2ROM.py -a ~/Games/UEFs/games/A-M/DAR-Draughts_E.uef ROMs/Draughts.rom
 ./UEF2ROM.py -a -s -t -w 39f ~/Games/UEFs/games/A-M/Elixir_E.uef ROMs/Elixir-1.rom ROMs/Elixir-2.rom
@@ -36,52 +36,74 @@ set -e
 ./UEF2ROM.py -a -b -t -w 880 -s ~/Games/UEFs/games/A-M/Firetrack_E.uef ROMs/Firetrack-1.rom ROMs/Firetrack-2.rom
 ./UEF2ROM.py -a -b -s ~/Games/UEFs/games/A-M/Frak_Tape.uef ROMs/Frak-1.rom ROMs/Frak-2.rom
 ./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/A-M/Freefall_E.uef ROMs/Freefall-1.rom ROMs/Freefall-2.rom
+./UEF2ROM.py -a -m ~/Games/UEFs/games/A-M/Frenzy_E.uef ROMs/Frenzy.rom
 ./UEF2ROM.py -a -b -m -s ~/Games/UEFs/Gauntlet_E.uef ROMs/Gauntlet-1.rom ROMs/Gauntlet-2.rom
 ./UEF2ROM.py -a -b -m ~/Games/UEFs/games/A-M/Go_E.uef ROMs/Go-1.rom ROMs/Go-2.rom
 ./UEF2ROM.py -a -b -w 39f -t ~/Games/UEFs/games/A-M/Guardian_E.uef ROMs/Guardian.rom
+./UEF2ROM.py -a -t -s -w 39f ~/Games/UEFs/games/A-M/Gyroscope_E.uef ROMs/Gyroscope-1.rom ROMs/Gyroscope-2.rom
+./UEF2ROM.py -a -m -s ~/Games/UEFs/games/A-M/Hobgoblin_E.uef ROMs/Hobgoblin-1.rom ROMs/Hobgoblin-2.rom
+./UEF2ROM.py -a -m -s ~/Games/UEFs/games/A-M/Hobgoblin2_E.uef ROMs/Hobgoblin2-1.rom ROMs/Hobgoblin2-2.rom
 ./UEF2ROM.py -a -b -w 860 ~/Games/UEFs/games/A-M/HorseRace.uef ROMs/HorseRace.rom
+./UEF2ROM.py -a -m ~/Games/UEFs/games/A-M/Hunchback-Ocean_E.uef ROMs/Hunchback.rom
 ./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/A-M/Impact.uef ROMs/Impact-1.rom ROMs/Impact-2.rom
 ./UEF2ROM.py -a -b -w 39f -t -s ~/Games/UEFs/games/A-M/JetBootJack_E.uef ROMs/JetBootJack-1.rom ROMs/JetBootJack-2.rom
 ./UEF2ROM.py -a -b -w 39f ~/Games/UEFs/games/A-M/Joust.uef ROMs/Joust-1.rom ROMs/Joust-2.rom
 ./UEF2ROM.py -a -b -m ~/Games/UEFs/games/A-M/JungleJive_Tape.uef ROMs/JungleJive.rom
 ./UEF2ROM.py -a -b -s ~/Games/UEFs/games/A-M/KarateCombat.uef ROMs/KarateCombat-1.rom ROMs/KarateCombat-2.rom
 ./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/KillerGorilla.uef ROMs/KillerGorilla.rom
+./UEF2ROM.py -a -s -w 39f ~/Games/UEFs/games/A-M/KillerGorilla2-PIAS_E.uef ROMs/KillerGorilla2-1.rom ROMs/KillerGorilla2-2.rom
 ./UEF2ROM.py -a -b -w 120 -s -t ~/Games/UEFs/games/A-M/KissinCousins_E.uef ROMs/KissinCousins-1.rom ROMs/KissinCousins-2.rom
 ./UEF2ROM.py -a -b -r -s -m ~/Games/UEFs/games/A-M/LastOfTheFree.uef ROMs/LastOfTheFree-1.rom ROMs/LastOfTheFree-2.rom
+./UEF2ROM.py -a -m -s ~/Games/UEFs/games/A-M/Locomotion_E.uef ROMs/Locomotion-1.rom ROMs/Locomotion-2.rom
 ./UEF2ROM.py -a -b -m ~/Games/UEFs/games/A-M/LunarRescue_E.uef ROMs/LunarRescue.rom
 ./UEF2ROM.py -a -b -w 880 ~/Games/UEFs/games/A-M/MagicMushrooms_Tape.uef ROMs/MagicMushrooms.rom
 ./UEF2ROM.py -a -b -m ~/Games/UEFs/games/A-M/Maze_E.uef ROMs/Maze.rom
 ./UEF2ROM.py -a -b ~/Games/UEFs/games/A-M/Meteors_E.uef ROMs/Meteors.rom
+./UEF2ROM.py -a -m ~/Games/UEFs/games/A-M/Microball_BE.uef ROMs/Microball.rom
+./UEF2ROM.py -a -p -t -w 39f -s ~/Games/UEFs/games/A-M/Mikie_E.uef ROMs/Mikie-1.rom ROMs/Mikie-2.rom
 ./UEF2ROM.py -a -b -w 880 ~/Games/UEFs/games/A-M/Monsters.uef ROMs/Monsters.rom
 ./UEF2ROM.py -a -b -w 39f ~/Games/UEFs/games/A-M/MoonBuggy.uef ROMs/MoonBuggy-1.rom ROMs/MoonBuggy-2.rom
 ./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/MoonRaider_E.uef ROMs/MoonRaider-1.rom ROMs/MoonRaider-2.rom
+./UEF2ROM.py -a -w 39f ~/Games/UEFs/games/N-Z/NightmareMaze-MRM_E.uef ROMs/NightmareMaze.rom
 ./UEF2ROM.py -a -b ~/Games/UEFs/games/N-Z/Overdrive.uef ROMs/Overdrive.rom
 ./UEF2ROM.py -a -b -w 120 ~/Games/UEFs/games/N-Z/Pengi.uef ROMs/Pengi.rom
 ./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/N-Z/PhilosophersQuest_E.uef ROMs/PhilosophersQuest-1.rom ROMs/PhilosophersQuest-2.rom
 ./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/N-Z/PhilosophersStone_BE.uef ROMs/PhilosophersStone-1.rom ROMs/PhilosophersStone-2.rom
 ./UEF2ROM.py -a -b -w 120 ~/Games/UEFs/games/N-Z/Pinball.uef ROMs/Pinball.rom
+./UEF2ROM.py -a -m -s ~/Games/UEFs/games/N-Z/PlanB_BE.uef ROMs/PlanB-1.rom ROMs/PlanB-2.rom
+./UEF2ROM.py -a -s ~/Games/UEFs/games/N-Z/PlanB2_BE.uef ROMs/PlanB2-1.rom ROMs/PlanB2-2.rom
+./UEF2ROM.py -a -m ~/Games/UEFs/games/N-Z/Planetoid_E.uef ROMs/Planetoid.rom
+./UEF2ROM.py -a -s ~/Games/UEFs/games/N-Z/Predator_E.uef ROMs/Predator-1.rom ROMs/Predator-2.rom
 ./UEF2ROM.py -a -b -w 39f -s -t ~/Games/UEFs/games/N-Z/Psycastria.uef ROMs/Psycastria-1.rom ROMs/Psycastria-2.rom
 ./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Quest.uef ROMs/Quest-1.rom ROMs/Quest-2.rom
+./UEF2ROM.py -a -s ~/Games/UEFs/games/N-Z/Qwak_E.uef ROMs/Qwak-1.rom ROMs/Qwak-2.rom
 ./UEF2ROM.py -a -b -m ~/Games/UEFs/games/N-Z/Ravage_E.uef ROMs/Ravage.rom
 ./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Repton.uef ROMs/Repton-1.rom ROMs/Repton-2.rom
 ./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Repton2.uef ROMs/Repton2-1.rom ROMs/Repton2-2.rom
 ./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Repton3.uef ROMs/Repton3-1.rom ROMs/Repton3-2.rom
 ./UEF2ROM.py -a ~/Games/UEFs/games/A-M/DAR-Reversi_E.uef ROMs/Reversi.rom
+./UEF2ROM.py -a -m ~/Games/UEFs/games/N-Z/RigAttack_E.uef ROMs/RigAttack.rom
 ./UEF2ROM.py -a -b ~/Games/UEFs/games/N-Z/RubbleTrouble.uef ROMs/RubbleTrouble-1.rom ROMs/RubbleTrouble-2.rom
 ./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/N-Z/Saracoid.uef ROMs/Saracoid-1.rom ROMs/Saracoid-2.rom
 ./UEF2ROM.py -a -b -m -s ~/Games/UEFs/Shark_E.uef ROMs/Shark-1.rom ROMs/Shark-2.rom
 ./UEF2ROM.py -a -b -w 880 ~/Games/UEFs/games/N-Z/Sim_Tape.uef ROMs/Sim.rom
 ./UEF2ROM.py -a -b ~/Games/UEFs/games/N-Z/SmashAndGrab.uef ROMs/SmashAndGrab.rom
 ./UEF2ROM.py -a -b -m ~/Games/UEFs/games/N-Z/Snooker-Acornsoft_E.uef ROMs/Snooker.rom
+./UEF2ROM.py -a -s -t -w 39f ~/Games/UEFs/games/N-Z/SpaceAgentZeldaMeetsTheBugEyes_E.uef ROMs/SpaceAgentZeldaMeetsTheBugEyes-1.rom ROMs/SpaceAgentZeldaMeetsTheBugEyes-2.rom
 ./UEF2ROM.py -a -b -t -s ~/Games/UEFs/games/N-Z/SpacemanSid.uef ROMs/SpacemanSid-1.rom ROMs/SpacemanSid-2.rom
 ./UEF2ROM.py -a -b -s -m ~/Games/UEFs/games/N-Z/SphinxAdventure_E.uef ROMs/SphinxAdventure-1.rom ROMs/SphinxAdventure-2.rom
 ./UEF2ROM.py -a -b ~/Games/UEFs/games/N-Z/StockCar.uef ROMs/StockCar-1.rom ROMs/StockCar-2.rom
 ./UEF2ROM.py -a -b -w 39f -t ~/Games/UEFs/games/N-Z/Stranded_E.uef ROMs/Stranded.rom
+./UEF2ROM.py -a -m -s ~/Games/UEFs/games/N-Z/SupermanTheGame_BE.uef ROMs/SupermanTheGame-1.rom ROMs/SupermanTheGame-2.rom
+./UEF2ROM.py -a -s -t -w 39f ~/Games/UEFs/games/N-Z/Survivors_BE.uef ROMs/Survivors-1.rom ROMs/Survivors-2.rom
 ./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Syncron.uef ROMs/Syncron-1.rom ROMs/Syncron-2.rom
 ./UEF2ROM.py -a -b -m ~/Games/UEFs/games/N-Z/TalesOfTheArabianNights_E.uef ROMs/TalesOfTheArabianNights-1.rom ROMs/TalesOfTheArabianNights-2.rom
 ./UEF2ROM.py -a -b -s ~/Games/UEFs/games/N-Z/Thrust_Tape.uef ROMs/Thrust-1.rom ROMs/Thrust-2.rom
+./UEF2ROM.py -a -r -s -w 39f ~/Games/UEFs/games/N-Z/Thunderstruck_E.uef ROMs/Thunderstruck-1.rom ROMs/Thunderstruck-2.rom
+./UEF2ROM.py -a -m -r -s ~/Games/UEFs/games/N-Z/Thunderstruck2_E.uef ROMs/Thunderstruck2-1.rom ROMs/Thunderstruck2-2.rom
 ./UEF2ROM.py -a -b -m -s ~/Games/UEFs/games/N-Z/TwinKingdomValley-modified_E.uef ROMs/TwinKingdomValley-1.rom ROMs/TwinKingdomValley-2.rom
 ./UEF2ROM.py -a -b -s -l ~/Games/UEFs/games/N-Z/TwelfthNight-Electron.uef ROMs/TwelfthNight-1.rom ROMs/TwelfthNight-2.rom
 ./UEF2ROM.py -a -b -w 120 ~/Games/UEFs/games/N-Z/VideosRevenge.uef ROMs/VideosRevenge-1.rom ROMs/VideosRevenge-2.rom
 ./UEF2ROM.py -a -b -s -m ~/Games/UEFs/games/N-Z/Village.uef ROMs/Village-1.rom ROMs/Village-2.rom
 ./UEF2ROM.py -a -b -s -m ~/Games/UEFs/games/N-Z/WheelOfFortune_E.uef ROMs/WheelOfFortune-1.rom ROMs/WheelOfFortune-2.rom
+./UEF2ROM.py -a -s ~/Games/UEFs/games/N-Z/Zalaga-PIASRR_E.uef ROMs/Zalaga-1.rom ROMs/Zalaga-2.rom
--- testroms.sh
+++ testroms.sh
@@ -36,52 +36,74 @@ set -e
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Firetrack-1.rom -rom1 ROMs/Firetrack-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Frak-1.rom -rom1 ROMs/Frak-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Freefall-1.rom -rom1 ROMs/Freefall-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Frenzy.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Gauntlet-1.rom -rom1 ROMs/Gauntlet-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Go-1.rom -rom1 ROMs/Go-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Guardian.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Gyroscope-1.rom -rom1 ROMs/Gyroscope-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Hobgoblin-1.rom -rom1 ROMs/Hobgoblin-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Hobgoblin2-1.rom -rom1 ROMs/Hobgoblin2-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/HorseRace.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Hunchback.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Impact-1.rom -rom1 ROMs/Impact-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/JetBootJack-1.rom -rom1 ROMs/JetBootJack-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Joust-1.rom -rom1 ROMs/Joust-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/JungleJive.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/KarateCombat-1.rom -rom1 ROMs/KarateCombat-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/KillerGorilla.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/KillerGorilla2-1.rom -rom1 ROMs/KillerGorilla2-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/KissinCousins-1.rom -rom1 ROMs/KissinCousins-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/LastOfTheFree-1.rom -rom1 ROMs/LastOfTheFree-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Locomotion-1.rom -rom1 ROMs/Locomotion-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/LunarRescue.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/MagicMushrooms.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Maze.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Meteors.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Microball.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Mikie-1.rom -rom1 ROMs/Mikie-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Monsters.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/MoonBuggy-1.rom -rom1 ROMs/MoonBuggy-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/MoonRaider-1.rom -rom1 ROMs/MoonRaider-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/NightmareMaze.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Overdrive.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Pengi.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/PhilosophersQuest-1.rom -rom1 ROMs/PhilosophersQuest-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/PhilosophersStone-1.rom -rom1 ROMs/PhilosophersStone-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Pinball.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/PlanB-1.rom -rom1 ROMs/PlanB-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/PlanB2-1.rom -rom1 ROMs/PlanB2-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Planetoid.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Predator-1.rom -rom1 ROMs/Predator-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Psycastria-1.rom -rom1 ROMs/Psycastria-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Quest-1.rom -rom1 ROMs/Quest-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Qwak-1.rom -rom1 ROMs/Qwak-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Ravage.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Repton-1.rom -rom1 ROMs/Repton-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Repton2-1.rom -rom1 ROMs/Repton2-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Repton3-1.rom -rom1 ROMs/Repton3-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Reversi.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/RigAttack.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/RubbleTrouble-1.rom -rom1 ROMs/RubbleTrouble-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Saracoid-1.rom -rom1 ROMs/Saracoid-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Shark-1.rom -rom1 ROMs/Shark-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Sim.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/SmashAndGrab.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Snooker.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/SpaceAgentZeldaMeetsTheBugEyes-1.rom -rom1 ROMs/SpaceAgentZeldaMeetsTheBugEyes-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/SpacemanSid-1.rom -rom1 ROMs/SpacemanSid-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/SphinxAdventure-1.rom -rom1 ROMs/SphinxAdventure-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/StockCar-1.rom -rom1 ROMs/StockCar-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Stranded.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/SupermanTheGame-1.rom -rom1 ROMs/SupermanTheGame-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Survivors-1.rom -rom1 ROMs/Survivors-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Syncron-1.rom -rom1 ROMs/Syncron-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/TalesOfTheArabianNights-1.rom -rom1 ROMs/TalesOfTheArabianNights-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Thrust-1.rom -rom1 ROMs/Thrust-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Thunderstruck-1.rom -rom1 ROMs/Thunderstruck-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Thunderstruck2-1.rom -rom1 ROMs/Thunderstruck2-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/TwinKingdomValley-1.rom -rom1 ROMs/TwinKingdomValley-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/TwelfthNight-1.rom -rom1 ROMs/TwelfthNight-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/VideosRevenge-1.rom -rom1 ROMs/VideosRevenge-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Village-1.rom -rom1 ROMs/Village-2.rom
 /data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/WheelOfFortune-1.rom -rom1 ROMs/WheelOfFortune-2.rom
+/data/david/Software/Emulation/elkulator/elkulator -rom2 ROMs/Zalaga-1.rom -rom1 ROMs/Zalaga-2.rom
