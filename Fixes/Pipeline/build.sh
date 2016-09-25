b4afda7f426ae5725366b8bd7ae753ea0f4e5e5a
David Boddie <david@boddie.org.uk>
2016-09-25 02:39:11 +0200

    Patched out a *TAPE call in the regular Pipeline UEF's GAME executable.

--- Fixes/Pipeline/build.sh
+++ Fixes/Pipeline/build.sh
@@ -10,6 +10,7 @@ fi
 UEFtrans.py $1 extract 0,1,2,3 temp
 ophis -o temp/LOADER loader.oph
 python -c 'open("temp/LOADER.inf", "w").write("$.LOADER\t1900\t1900\t%x" % len(open("temp/LOADER").read()))'
+python -c 't = open("temp/GAME").read(); t = t[:0x451b] + "\xea\xea\xea" + t[0x451e:]; open("temp/GAME", "w").write(t)'
 UEFtrans.py $2 new Electron 0
 UEFtrans.py $2 append temp/LOADER,temp/GAME,temp/IO
 rm -r temp
