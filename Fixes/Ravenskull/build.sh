c91581e6dd648423cb306bff05683842d4bcedd4
David Boddie <david@boddie.org.uk>
2019-05-11 01:25:47 +0200

    Fine tune a title to include another original file.

--- Fixes/Ravenskull/build.sh
+++ Fixes/Ravenskull/build.sh
@@ -11,10 +11,9 @@ UEFtrans.py "$1" extract 0,1,2,3,4,5,6,7 temp
 #./raven.py
 #distance_pair.py --compress temp/SCREEN temp/SCRDATA
 #ophis -o temp/screen screen.oph
-ophis -o temp/LOADER loader.oph
+#ophis -o temp/LOADER loader.oph
 #cat temp/screen temp/SCRDATA > temp/SCREEN
-#python -c 'open("temp/SCREEN.inf", "w").write("$.SCREEN\te00\te00\t%x" % len(open("temp/SCREEN").read()))'
-python -c 'open("temp/LOADER.inf", "w").write("$.LOADER\t1900\t1900\t%x" % len(open("temp/LOADER").read()))'
+python -c 'open("temp/SCREEN.inf", "w").write("$.SCREEN\t2700\t2717\t2900")'
 UEFtrans.py "$2" new Electron 0
-UEFtrans.py "$2" append temp/LOADER,temp/SCREEN,temp/DATA,temp/DAT1,temp/DAT2,temp/MAIN
+UEFtrans.py "$2" append temp/CONTROL,temp/SCREEN,temp/DATA,temp/DAT1,temp/DAT2,temp/MAIN
 rm -r temp
