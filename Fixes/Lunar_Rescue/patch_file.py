990c342852627c8f1f3283752b2a0a15c55d09e1
David Boddie <david@boddie.org.uk>
2024-03-04 01:12:49 +0100

    Renamed a file for easier wrangling.

--- Fixes/Lunar_Rescue/patch_file.py
+++ Fixes/Lunar_Rescue/patch_file.py
@@ -2,10 +2,18 @@
 
 import os, shlex, sys
 
+# Change the file name of the last file in the penultimate file.
+t = open("temp/Lunar2", "rb").read()
+t = b"Lunar33" + t[7:]
+open("temp/Lunar2", "wb").write(t)
+
 # Patch out the JMP (&FFFC) reset call EORed with 0x0e in the final file.
 # 0x04 is 0xea ^ 0x0e where 0xea is the opcode for NOP.
 t = open("temp/Lunar3\xc3", "rb").read()
 t = t[:0x261e] + b"\x04\x04\x04" + t[0x2621:]
-open("temp/Lunar3\xc3", "wb").write(t)
+open("temp/Lunar33", "wb").write(t)
 
-os.system("UEFtrans.py " + shlex.quote(sys.argv[1]) + " insert 3 temp/Lunar3\xc3")
+t = open("temp/Lunar3\xc3.inf", "rb").read()
+open("temp/Lunar33.inf", "wb").write(t)
+
+os.system("UEFtrans.py " + shlex.quote(sys.argv[1]) + " insert 3 temp/Lunar33")
